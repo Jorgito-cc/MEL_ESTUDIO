41eec84a2690967ecb92abb3fbb697e81942658b
jorge choque <jorgitochoque007@gmail.com>
2025-06-10 15:54:36 -0400

    feat(usuario):imagen en componente  Sobremi

--- src/presentation/pages/SobreMi.jsx
+++ src/presentation/pages/SobreMi.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/page/sobremi.css';
-import imagen2 from '../../assets/imagen2.jpg';
+import imagen2 from '../../assets/melisa.jpeg';
 
 export const SobreMi = () => {
   return (
